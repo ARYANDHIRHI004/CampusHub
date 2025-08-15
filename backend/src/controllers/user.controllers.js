9e4a6d628ba1e9badc964eaeb10e86fd69e93906
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-08-15 19:03:16 +0530

    feat: Login user controller done

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -4,6 +4,14 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import crypto from "crypto";
 import { emailVerificationMailGenContent, sendMail } from "../utils/mail.js";
 
+const generateAccessAndRefreshToken = asyncHandler(async (user) => {
+  const accessTokne = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+  user.refreshToken = refreshToken;
+  await user.save();
+  return { accessTokne, refreshToken };
+});
+
 export const registerUser = asyncHandler(async (req, res) => {
   const { fullname, email, username, password } = req.body;
 
@@ -41,7 +49,37 @@ export const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, "User registered successfully", {}));
 });
 
-export const loginUser = asyncHandler(async (req, res) => {});
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  const user = await User.findOne({
+    $or: [{ email }, { username }],
+  }).select("-password -resfreshToken -emailVerificationToken");
+
+  if (!user) {
+    throw new ApiError(401, "User does not exist.");
+  }
+
+  const isMatched = await user.comparePassword(password);
+
+  if (!isMatched) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user,
+  );
+
+  const options = {
+    httpOnly: true,
+  };
+
+  return req
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(201, "User logged In", user));
+});
 
 export const logoutUser = asyncHandler(async (req, res) => {});
 export const getcurrentUser = asyncHandler(async (req, res) => {});
